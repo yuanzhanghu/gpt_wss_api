05909945b0f6de81ea484b2e3c66bbe42d67c99f
Yuanzhang Hu <huyuanzhang@gmail.com>
2024-04-29 06:46:59 -0400

    updated test example

--- test_websocket.py
+++ test_websocket.py
@@ -1,41 +1,6 @@
 import asyncio
 from gpt_wss_api import WebSocketClient
 
-class WSS:
-    def __init__(self, url=None, headers=None):
-        self.url = url
-        self.headers = headers
-        self.sockClient = WebSocketClient(url=self.url, headers=self.headers)
-
-    async def connect(self):
-        try:
-            ret = await self.sockClient.connect()
-            if ret:
-                print('Connected successfully')
-                return True
-            else:
-                print('Failed to connect!')
-                return False
-        except Exception as e:
-            print(f"Error connecting to WebSocket: {e}")
-            return False
-
-    async def send(self, message):
-        if await self.sockClient.isConnected():
-            await self.sockClient.send(message)
-
-    async def recv(self):
-        try:
-            while await self.sockClient.isConnected():
-                message = await self.sockClient.recv()
-                print(message)
-                await asyncio.sleep(0.1)
-        except Exception as e:
-            print(f"Error receiving data: {e}")
-
-    async def close(self):
-        await self.sockClient.close()
-
 async def main():
     url = 'wss://chatgpt-async-webps-prod-southcentralus-5.chatgpt.com/client/hubs/conversations?access_token=eyJhbGcixxxxxxxxxx'
     headers = {
@@ -43,13 +8,27 @@ async def main():
         'Connection': 'Upgrade',
         'Upgrade': 'websocket',
         'Sec-WebSocket-Version': '13',
-        'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+        'Sec-WebSocket-Extensions': 'permessage-deflate',
     }
-    wss = WSS(url, headers)
-    if await wss.connect():
-        # asyncio.create_task(wss.send('Hello WebSocket!'))
-        await wss.recv()
-    await wss.close()
+    sockClient = WebSocketClient(url=url, headers=headers)
+    
+    try:
+        if await sockClient.connect():
+            print('Connected successfully')
+            try:
+                while await sockClient.isConnected():
+                    message = await sockClient.recv()
+                    print(message)
+                    await asyncio.sleep(0.1)
+            except Exception as e:
+                print(f"Error receiving data: {e}")
+        else:
+            print('Failed to connect!')
+    except Exception as e:
+        print(f"Error connecting to WebSocket: {e}")
+    finally:
+        await sockClient.close()
+        print("Connection closed.")
 
 if __name__ == '__main__':
     asyncio.run(main())
